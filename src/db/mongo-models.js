704990328b867b39650483ba099e49ddd54dd8a5
mithun <mithunkarthick3@gmail.com>
2021-12-17 21:58:30 +0530

    backup commit

--- src/db/mongo-models.js
+++ src/db/mongo-models.js
@@ -2,22 +2,22 @@ const mongoose = require("mongoose");
 const userSchema = require("./schema/user-schema");
 
 const getMongoConnection = async (modelName) => {
-	const mongoConnection = await mongoose.createConnection(
-		"mongodb+srv://theMyth333:Myth_333@notes-cuqms.mongodb.net/woodsDB?retryWrites=true&w=majority",
-		{
-			useNewUrlParser: true,
-			bufferCommands: false,
-			bufferMaxEntries: 0,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		}
-	);
+  const mongoConnection = await mongoose.createConnection(
+    "mongodb+srv://theMyth333:Myth_333@notes-cuqms.mongodb.net/woodsDB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  );
 
-	if (mongoConnection.readyState === 1) {
-		const User = mongoConnection.model("user", userSchema, "users"); //3rd collection name
-		console.log("Connected to mongoDB");
-		return { User };
-	}
+  if (mongoConnection.readyState === 1) {
+    const User = mongoConnection.model("user", userSchema, "users"); //3rd collection name
+    console.log("Connected to mongoDB");
+    return { User };
+  }
 };
 
 module.exports = getMongoConnection;
